3293e4da5fda27a48526f68730e927009b5f4f69
ajayvarghese2000 <58085441+ajayvarghese2000@users.noreply.github.com>
2021-12-24 14:05:18 +0000

    created main file and comments.

---
+++ main.py
@@ -0,0 +1,12 @@
+#
+#  _____ _                 _       _           _       ______                      
+# /  ___(_)               | |     | |         | |      |  _  \                     
+# \ `--. _ _ __ ___  _   _| | __ _| |_ ___  __| |______| | | |_ __ ___  _ __   ___ 
+#  `--. \ | '_ ` _ \| | | | |/ _` | __/ _ \/ _` |______| | | | '__/ _ \| '_ \ / _ \
+# /\__/ / | | | | | | |_| | | (_| | ||  __/ (_| |      | |/ /| | | (_) | | | |  __/
+# \____/|_|_| |_| |_|\__,_|_|\__,_|\__\___|\__,_|      |___/ |_|  \___/|_| |_|\___|
+#
+#
+# This program is used to simulate the final drones communications to the cloud server
+# Will be used to test the servers endpoints and the GUI's representation of data without
+# the need of the physicall drone built
